6e97557518381b98e5510295aa0d36843e60a196
xcaim04 <87400766+CarlosJavier4@users.noreply.github.com>
2022-12-30 05:56:12 -0500

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -1,94 +1,81 @@
+from time import sleep
 import atributes
-import calcAtk
-import pokeball
-import check
-import time
+import containerPokemon
 
 print('-----------------Start the battle-------------------')
 
 # Choose the enemy pokemon
-idx = pokeball.pos() 
-name_pokemon_oponent = pokeball.oponent[idx][0] 
-type_pokemon_oponent = pokeball.oponent[idx][1]  
-atk_pokemon_oponent = pokeball.oponent[idx][2]
-def_pokemon_oponent = pokeball.oponent[idx][3]
-vel_pokemon_oponent = pokeball.oponent[idx][4]
+idx = containerPokemon.pos() 
+name_pokemon_oponent = containerPokemon.oponent[idx].imp()[0] 
+type_pokemon_oponent = containerPokemon.oponent[idx].imp()[1] 
+atk_pokemon_oponent = containerPokemon.oponent[idx].imp()[2] 
+def_pokemon_oponent = containerPokemon.oponent[idx].imp()[3] 
+vel_pokemon_oponent = containerPokemon.oponent[idx].imp()[4] 
 
 print(f'\t ==> Your oponent has sent {name_pokemon_oponent}')
 print(f'\t Type: {type_pokemon_oponent}')
 print(f'\t Atk: {atk_pokemon_oponent}   Def: {def_pokemon_oponent}')
-
-time.sleep(1)
-
+sleep(1)
 print('\nQuickly, choose your pokemon to defend yourself.\n')
-time.sleep(2)
+sleep(2)
 
 cont = 1
 
 # Choose your pokemon
-for i in pokeball.pokemon:
-    print(f'\t ==> {cont}.{i[0]} \n\t type: {i[1]}  //  Atk: {i[2]}  //   Def: {i[3]} \n')
+cantPokemon = len(containerPokemon.pokeball)
+
+for i in range(0, cantPokemon):
+    aux = containerPokemon.pokeball[i].imp()
+    print(f'\t ==> {cont}.{aux[0]} \n\t type: {aux[1]}  //  Atk: {aux[2]}  //   Def: {aux[3]} \n')
     cont = cont + 1
-    time.sleep(1.6)
+    sleep(1.6)
     
 position = int(input('-> Select the number: '))
-position = check.checkPosition(position, cont)
+position = atributes.checkPosition(position, cont)
 
-name_pokemon = pokeball.pokemon[position - 1][0]
-type_pokemon = pokeball.pokemon[position - 1][1]
-atk_pokemon = pokeball.pokemon[position - 1][2]
-def_pokemon = pokeball.pokemon[position - 1][3]
-vel_pokemon = pokeball.pokemon[position - 1][4]
+name_pokemon = containerPokemon.pokeball[position - 1].imp()[0]
+type_pokemon = containerPokemon.pokeball[position - 1].imp()[1]
+atk_pokemon = containerPokemon.pokeball[position - 1].imp()[2]
+def_pokemon = containerPokemon.pokeball[position - 1].imp()[3]
+vel_pokemon = containerPokemon.pokeball[position - 1].imp()[4]
 
 print(f'Great!! {name_pokemon} is an excellent option')
-time.sleep(0.50)
+sleep(0.50)
 print('\n')
 
 # Who attack...
 gamer = atributes.WhoAttack(name_pokemon, vel_pokemon, name_pokemon_oponent, vel_pokemon_oponent)
 
-life_pokemon, life_oponent = 60, 60
+life_pokemon, life_oponent = 60, 60  #Using a tuple
 
 while life_pokemon > 0 and life_oponent > 0:
 
     print(f'{gamer} prepares to attack...', end = ' ')
-    time.sleep(0.50)
+    sleep(0.50)
 
-    ptos = calcAtk.battle(type_pokemon, type_pokemon_oponent, atk_pokemon, def_pokemon_oponent)
+    ptos = atributes.battle(type_pokemon, type_pokemon_oponent, atk_pokemon, def_pokemon_oponent)
     print(f'{gamer} dealt {ptos} damage...', end=' ')
     
-    time.sleep(1)
-    
-    a, b = 0, 0
+    sleep(1)
     
     if gamer == name_pokemon:
-        a = name_pokemon_oponent
+        swap_name = name_pokemon_oponent
         life_oponent = life_oponent - ptos
         if life_oponent > 0: 
-            print(f'{a} hit points are: {life_oponent}\n')
+            print(f'{swap_name} hit points are: {life_oponent}\n')
     elif gamer == name_pokemon_oponent:
-        a = name_pokemon
+        swap_name = name_pokemon
         life_pokemon = life_pokemon - ptos
         if life_pokemon > 0: 
-            print(f'{a} hit points are: {life_pokemon}\n')
+            print(f'{swap_name} hit points are: {life_pokemon}\n')
 
     # Swap the names
-    b = gamer
-    gamer = a
-    a = b
-
+    gamer, swap_name = swap_name, gamer
     # Swap the types
-    aux = type_pokemon
-    type_pokemon = type_pokemon_oponent
-    type_pokemon_oponent = aux
+    type_pokemon, type_pokemon_oponent = type_pokemon_oponent, type_pokemon
     
 
 if life_pokemon <= 0:
     print(f'\n==> {name_pokemon} has lost\n\n')
 else:
     print(f'\n==> {name_pokemon_oponent} has lost\n\n')
-
-
-
-
-
